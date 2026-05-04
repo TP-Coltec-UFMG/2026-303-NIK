f970ee3b548fcd4231d9c8796c49734379f2da48
Gabriel Della Gaspera <gabriel.gaspera@gmail.com>
2026-05-04 19:07:34 -0300

    desculpa andré
    mudei a funcao de tremer, ficou uma bosta, mas a antiga ta comentada entao tanto faz.
    
    botei uma configuracao de tirar a tremida pq eu acho melhor sem, mas se colocar tremida pode ter tirar como acessibilidade. 🧠🧠🧠

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -7,35 +7,46 @@ extends Camera3D
 
 var posicao_real : Vector3 = position
 
-var i : float = 0;
-var intensidade_animacao : float = 0
+var balanco : float = 0;
+var tempo_balanco : float = 0;
+var offset_balanco : Vector3 = Vector3.ZERO
+var offset_balanco_alvo : Vector3 = Vector3.ZERO
+var intensidade_balanco = 30
+var balanco_camera : bool = false
 
 func _process(delta: float) -> void:
-	var posicao_alvo = offset + alvo.position
-	position.x = lerpf(position.x, posicao_alvo.x, delta * velocidade)
-	position.y = lerpf(position.y, posicao_alvo.y, delta * velocidade)
-	position.z = lerpf(position.z, posicao_alvo.z, delta * velocidade)
+	var posicao_alvo = alvo.position
+	# position.x = lerpf(position.x, posicao_alvo.x, delta * velocidade)
+	# position.y = lerpf(position.y, posicao_alvo.y, delta * velocidade)
+	# position.z = lerpf(position.z, posicao_alvo.z, delta * velocidade)
 
+	# posicao_real = Vector3(
+	# 	lerpf(posicao_real.x, posicao_alvo.x, delta * velocidade),
+	# 	lerpf(posicao_real.y, posicao_alvo.y, delta * velocidade),
+	# 	lerpf(posicao_real.z, posicao_alvo.z, delta * velocidade)
+	# )
 
-	posicao_real = Vector3(
-		lerpf(posicao_real.x, posicao_alvo.x, delta * velocidade),
-		lerpf(posicao_real.y, posicao_alvo.y, delta * velocidade),
-		lerpf(posicao_real.z, posicao_alvo.z, delta * velocidade)
-	)
-
-	if get_tree().paused:
-		if intensidade_animacao < 0.05 and alvo.peso_animacao_andar > 0.1: # acabou de começar/parar o movimento
-			intensidade_animacao = alvo.peso_animacao_andar * 1
-		elif intensidade_animacao != 0 and alvo.peso_animacao_andar == 0: # parou de se mover
-			intensidade_animacao = max(intensidade_animacao - 2*delta, 0);
-		elif alvo.peso_animacao_andar > 0.1: # está se movendo
-			# vai, linearmente, aumentando a intensidade
-			intensidade_animacao = min(intensidade_animacao, alvo.peso_animacao_andar) + 16*delta;	
-		else: # não está se movendo
-			intensidade_animacao = max(intensidade_animacao - 4*delta, 0);
+	if not get_tree().paused:
+		# if intensidade_animacao < 0.05 and alvo.peso_animacao_andar > 0.1: # acabou de começar/parar o movimento
+		# 	intensidade_animacao = alvo.peso_animacao_andar * 1
+		# elif intensidade_animacao != 0 and alvo.peso_animacao_andar == 0: # parou de se mover
+		# 	intensidade_animacao = max(intensidade_animacao - 2*delta, 0);
+		# elif alvo.peso_animacao_andar > 0.1: # está se movendo
+		# 	# vai, linearmente, aumentando a intensidade
+		# 	intensidade_animacao = min(intensidade_animacao, alvo.peso_animacao_andar) + 16*delta;	
+		# else: # não está se movendo
+		# 	intensidade_animacao = max(intensidade_animacao - 4*delta, 0);
 	
-		var animacao_camera = Vector3(sin(i) * 0.1, abs(cos(i)) * 0.1, 0) * intensidade_animacao;
-		position = posicao_real + animacao_camera;
-		i += 6 * delta;
-	position = posicao_real;
+		offset_balanco = offset_balanco.lerp(offset_balanco_alvo, delta * 5)
+		if balanco >= tempo_balanco:
+			offset_balanco_alvo = Vector3((randf() - 0.5) * intensidade_balanco, (randf() - 0.5) * intensidade_balanco, 0)
+			tempo_balanco = (randf() * .15)
+			balanco = 0
+		balanco += randf() * 6 * delta;
+
+		posicao_alvo = posicao_alvo + offset + ((offset_balanco * alvo.peso_animacao_andar) if balanco_camera else Vector3.ZERO);
+	else:
+		posicao_alvo = posicao_alvo + offset_pausa;
+
+	position = position.lerp(posicao_alvo, delta * velocidade)
 	
