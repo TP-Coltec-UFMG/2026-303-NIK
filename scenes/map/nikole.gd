e7f9830d755fd4917a64d7c799bd8557447ee32b
Gabriel Della Gaspera <gabriel.gaspera@gmail.com>
2026-09-25 20:40:27 -0300

    novo fundo alex e alguns ajustes nas animcoes do mapa

--- scenes/map/nikole.gd
+++ scenes/map/nikole.gd
@@ -81,8 +81,8 @@ func animate(delta : float):
 
 	animation_progress += speed * delta * .035
 	
-	sprite.rotation = (sin(animation_progress) * 0.1) * walking_animation_weight + (sin(animation_progress / 4) * 0.01)
+	# sprite.rotation = (sin(animation_progress) * 0.1) * walking_animation_weight + (sin(animation_progress / 4) * 0.0)
 	sprite.scale.y = 1 - (sin(animation_progress * 2) * .01) * walking_animation_weight + -((1 + sin(animation_progress * .5)) * .01)
-	sprite.position.y = 0 + (-(1 + sin(animation_progress * 2 - PI / 2)) * 20.25) * walking_animation_weight
+	sprite.position.y = 0 + (-(1 + sin(animation_progress * 2 - PI / 2)) * 15.25) * walking_animation_weight
 
 	sprite.reset_physics_interpolation()
